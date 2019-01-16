39971e0063614b0d09c06bfba3045ba177242f0b
mikepcarlos <michaelpcarlos@gmail.com>
2019-01-16 13:47:24 -0500

    google auth passing through

--- server/index.js
+++ server/index.js
@@ -21,6 +21,11 @@ app.get(
   '/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
   })
-)
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google')
+);
 
 app.listen(5000);
