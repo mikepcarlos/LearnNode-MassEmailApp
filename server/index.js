f4837b76f486dac9827a7d79a1f49d856572d7e3
mikepcarlos <michaelpcarlos@gmail.com>
2019-01-16 13:03:09 -0500

    added passport dependancy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi:  'there' });
-});
+passport.use(new GoogleStrategy());
 
 app.listen(5000);
